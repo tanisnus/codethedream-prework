5834d6fe8b5fed96d0e059a4bf4dcc07eec948fc
tanisnus <taniss246@gmail.com>
2026-02-12 01:03:36 -0800

    Page Set up Clean Up

---
+++ pages/home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <section>
+            hi
+        </section>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
 import './App.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+
+
+const web_pages_router = createBrowserRouter([
+  { path: '/', 
+    element: <Home /> },
+])
 
 function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+  return <RouterProvider router={web_pages_router} />
 }
 
 export default App
